6c361b0c77d8930f283868afa4e5232f26c80e23
Sanjeev1412-official <sanjeevsnair1412official@gmail.com>
2026-08-24 10:09:56 +0530

    u

--- test_mpv.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: unused_local_variable
-import 'package:media_kit/media_kit.dart';
-
-void main() {
-  var player = Player();
-  var media = Media('url', extras: {'hls-bitrate': 'min'});
-}
